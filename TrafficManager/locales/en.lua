110e99b663a817420ce017610d4f5f4022c68a85
Focking Mikkel <mikkelfrav@gmail.com>
2022-12-09 20:35:09 +0100

    lortet

---
+++ TrafficManager/locales/en.lua
@@ -0,0 +1,53 @@
+Lang["en"] = {
+    notify = {
+        -- Brugt meget
+        ["notifyheader"] = "Traffic Control",
+
+        -- Åben trafik
+        ["opentraffic"] = "Traffic is now open",
+        -- Langsom trafik
+        ["slowtraffic"] = "Traffic is now slow",
+        -- Stoppet trafik'
+        ["stoppedtraffic"] = "Traffic has been stopped",
+    }
+
+    menu = {
+        -- Først mødte Menu
+        ["menuheader"] = "Traffic Control",
+        ["opentrafficmenu"] = "Open traffic",
+        ["slowtrafficmenu"] = "Slow traffic",
+        ["stoptrafficmenu"] = "Stop the traffic",
+
+        -- Langsom Trafik Menu
+        ["menuheader2"] = "Choose the size of the zone",
+        ["50slow"] = "50 Radius",
+        ["75slow"] = "75 Radius",
+        ["100slow"] = "100 Radius",
+        -- Stoppet Trafik Menu
+        ["menuheader3"] = "Choose the size of the zone",
+        ["50stop"] = "50 Radius",
+        ["75stop"] = "75 Radius",
+        ["100stop"] = "100 Radius",
+
+        -- Bruges flere gang :)
+        ["back"] = "<-- Back",
+    },
+
+
+    menudesc = {
+        -- Først mødte Menu
+        ["opentrafficdesc"] = "Traffic goes as normal!",
+        ["slowtrafficdesc"] = "Slows down the traffic!",
+        ["stoptrafficdesc"] = "Stops the traffic!",
+
+        -- Langsom Trafik Menu
+        ["50slowdesc"] = "Zone of 50 radius",
+        ["75slowdesc"] = "Zone of 75 radius",
+        ["100slow"] = "Zone of 100 radius",
+
+        -- Stoppet Trafik Menu
+        ["50stopdesc"] = "Zone of 50 radius",
+        ["75stopdesc"] = "Zone of 75 radius",
+        ["100stopdesc"] = "Zone of 100 radius",
+    }
+} 
